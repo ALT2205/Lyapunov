4d91b4cf2f72e4bd6124380d7ea41fe4efebef69
Adrien <acasteleiro@outlook.fr>
2020-02-05 12:31:53 +0100

    Fichier PPM crée pour afficher la fractale

--- Lyapu.cpp
+++ Lyapu.cpp
@@ -3,8 +3,19 @@
 #include <ctime>
 #include <cstdlib>
 #include <SDL2/SDL.h>
+#include <fstream>
 
-#define WINDOW 10
+#define WIDTH 600
+#define HEIGHT 600
+
+#define BORNEINFA 0.0
+#define BORNESUPA 4.0
+
+#define BORNEINFB 0.0
+#define BORNESUPB 4.0
+
+#define NUMOFITER 700
+
 #define X0 0.5
 
 //Couleur : Jaune si Exposant < 0
@@ -14,13 +25,17 @@
 // Quel logarithme doit-on utiliser ? LN ? LOG10 ? LOGN ? LOG2 ?
 // Quel bibliothèque graphique ? Une qui permet de modif pixels par pixels ? QT? OpenGL/SDL ? GTK+
 
-double scale(double borneSup, double borneInf) { // Echelle d'espacement entre chaque a/b pour x/y
-  return (borneSup-borneInf)/WINDOW + borneInf;
+double scaleA() { // Echelle d'espacement entre chaque a/b pour x/y
+  return ((BORNESUPA-BORNEINFA)/WIDTH); //+ BORNEINFA;
+}
+// FORMULE A CORRIGER
+double scaleB() {
+  return ((BORNESUPB-BORNEINFB)/HEIGHT); //+ BORNEINFB;
 }
 
 std::string AB(std::string seq){ // Complete la string de la taille de la FENETRE
   std::string toReturn;
-  while (toReturn.length() < WINDOW){
+  while (toReturn.length() < NUMOFITER){
     toReturn+=seq;
   }
   return toReturn;
@@ -38,27 +53,31 @@ double rN(char currChar,double a,double b){ // Choix entre A ou B selon le curre
   return ((currChar=='A')?a:b);
 }
 
-double **exposant(std::string seq, double scaleAB){
-  double ** arrayExpo = new double* [WINDOW]; // Creation d'un tableau en 2D afin de recuperer un tableau contenant les exposant
-  for (int i=0; i < WINDOW; i++)
-    arrayExpo[i] = new double[WINDOW];
+double **exposant(std::string seq){
+  double ** arrayExpo = new double* [WIDTH]; // Creation d'un tableau en 2D afin de recuperer un tableau contenant les exposant
+  for (int i=0; i < WIDTH; i++)
+    arrayExpo[i] = new double[HEIGHT];
   double a,b;
+  static double scaleOfA = scaleA();
+  std::cout << scaleOfA << std::endl;
+  static double scaleOfB = scaleB();
+  std::cout << scaleOfB << std::endl;
   double expoLyap;
-  for(int x = 0;x<WINDOW;++x){
-    for(int y = 0;y<WINDOW;++y){
-      a=aOrB(scaleAB,x);
-      b=aOrB(scaleAB,y);
+  for(int x = 0;x<WIDTH;++x){
+    for(int y = 0;y<HEIGHT;++y){
+      a=aOrB(scaleOfA,x);
+      b=aOrB(scaleOfB,y);
       expoLyap=0;
 
       double xn = X0;
       double rn = 0;
 
-      for(long unsigned int i=0; i<seq.length();++i){
-	xn = nextXN(xn,rn);
-	rn = rN(seq[i%seq.length()],a,b);
-	expoLyap += log(fabs(rn*(1-2*xn))/seq.length());
+      for(long unsigned int i=0; i<NUMOFITER;++i){
+	        rn = rN(seq[i],a,b);
+          xn = nextXN(xn,rn);
+	        expoLyap += log2(fabs(rn*(1-2*xn))) ;
       }
-      expoLyap /= seq.length();
+      expoLyap=expoLyap / NUMOFITER;
       arrayExpo[x][y]=expoLyap;
     }
   }
@@ -67,40 +86,42 @@ double **exposant(std::string seq, double scaleAB){
 
 
 int main(){ // Revoir l'implémentation graphique de Lyapu mais semble fonctionnelle au vue de la tete du tableau en 10 10 sur AAB;
-  /*SDL_Event event;
-  SDL_Renderer *renderer;
-  SDL_Window *window;
-  int i;
-
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_CreateWindowAndRenderer(WINDOW, WINDOW, 0, &window, &renderer);
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-  SDL_RenderClear(renderer);
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);*/
+            // Implementation du code en PNM PORTABLE PIXMAP A FAIRE : APPRENDRE ECRITURE FICHIER + LECTURE
   std::string seq = "AAB";
   seq=AB(seq);
-  double ** arrayOfLyapu=exposant(seq,scale(4,2.5));
-  for(int i=0;i<WINDOW;++i){ // Soucis dans
-    for(int j=0;j<WINDOW;++j){
-      /*if (tab[i][j]<0){
-	SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+  std::ofstream data("data.ppm"); // Création du fichier ou est stockée l'image
+  data << "P3";;
+  data << std::endl;
+  data << "600 600";
+  data << std::endl;
+  data << "255";
+  data << std::endl;
+
+  double ** arrayOfLyapu=exposant(seq);
+  for(int i=0;i<WIDTH;++i){
+    for(int j=0;j<HEIGHT;++j){
+      //std::cout << arrayOfLyapu[i][j];
+      if(arrayOfLyapu[i][j] < -2){
+        data << " 184 134 11 ";
       }
-      else{
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+      else if(arrayOfLyapu[i][j] >= -2 && arrayOfLyapu[i][j] < -1){
+        data << " 255 215 0 ";
       }
-      SDL_RenderDrawPoint(renderer, i, j);
+
+      else if(arrayOfLyapu[i][j] >= -1 && arrayOfLyapu[i][j] < -0.5){
+        data << " 255 228 54 ";
+      }
+      else if(arrayOfLyapu[i][j] >=0.5 && arrayOfLyapu[i][j] <0 ){
+        data << " 255 255 0";
+      }
+      else {
+        data << " 0 0 255";
+      }
     }
+    //std::cout << std::endl;
+    data << std::endl;
   }
-  while (1) {
-    if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
-      break;
-  }
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();*/
-  std::cout << arrayOfLyapu[i][j] << " ";
-    }
-    std::cout << std::endl;
-  }
+  free(arrayOfLyapu);
+  data.close();
   return EXIT_SUCCESS;
 }
