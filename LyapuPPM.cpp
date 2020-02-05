7599e930f1bcd92cfbe00b9fe5ea91df566ea792
Adrien <acasteleiro@outlook.fr>
2020-02-05 18:09:01 +0100

    Ajout du fichier LyapuPPM : image en PPM

--- LyapuPPM.cpp
+++ LyapuPPM.cpp
@@ -4,9 +4,10 @@
 #include <cstdlib>
 #include <SDL2/SDL.h>
 #include <fstream>
+#include <string>
 
-#define WIDTH 600
-#define HEIGHT 600
+#define WIDTH 1200
+#define HEIGHT 1200
 
 #define BORNEINFA 0.0
 #define BORNESUPA 4.0
@@ -24,6 +25,8 @@
 // Calcul de l'xposant de Lyapunov via https://www.youtube.com/watch?v=8xZyA09zRXY
 // Quel logarithme doit-on utiliser ? LN ? LOG10 ? LOGN ? LOG2 ?
 // Quel bibliothèque graphique ? Une qui permet de modif pixels par pixels ? QT? OpenGL/SDL ? GTK+
+
+// Degradé au format brut : Pourquoi ne pas passer par un fichier ?
 
 double scaleA() { // Echelle d'espacement entre chaque a/b pour x/y
   return ((BORNESUPA-BORNEINFA)/WIDTH); //+ BORNEINFA;
@@ -54,6 +57,17 @@ double rN(char currChar,double a,double b){ // Choix entre A ou B selon le curre
 }
 
 double **exposant(std::string seq){
+  std::ofstream data("data.ppm"); // Création du fichier ou est stockée l'image
+  data << "P3";
+  data << std::endl;
+  data << std::to_string(WIDTH);
+  data << " ";
+  data << std::to_string(HEIGHT);
+  data << std::endl;
+  data << "255";
+  data << std::endl;
+
+
   double ** arrayExpo = new double* [WIDTH]; // Creation d'un tableau en 2D afin de recuperer un tableau contenant les exposant
   for (int i=0; i < WIDTH; i++)
     arrayExpo[i] = new double[HEIGHT];
@@ -79,49 +93,69 @@ double **exposant(std::string seq){
       }
       expoLyap=expoLyap / NUMOFITER;
       arrayExpo[x][y]=expoLyap;
+
+      //LAMBDA NEGATIF
+
+      if(arrayExpo[x][y] < -4){
+        data << " 100 45 0 ";
+      }
+      else if(arrayExpo[x][y] >= -4 && arrayExpo[x][y] < -3.7){
+        data << " 112 62 0 ";
+      }
+      else if(arrayExpo[x][y] >= -3.7 && arrayExpo[x][y] < -3.4){
+        data << " 124 79 0 ";
+      }
+      else if(arrayExpo[x][y] >= -3.4 && arrayExpo[x][y] < -3.1){
+        data << "  136 96 0 ";
+      }
+      else if(arrayExpo[x][y] >= -3.1 && arrayExpo[x][y] < -2.9){
+        data << " 148 113 0 ";
+      }
+      else if(arrayExpo[x][y] >= -2.9 && arrayExpo[x][y] < -2.6){
+        data << " 160 130 0 ";
+      }
+      else if(arrayExpo[x][y] >= -2.6 && arrayExpo[x][y] < -2.3){
+        data << " 172 147 0 ";
+      }
+      else if(arrayExpo[x][y] >= -2.3 && arrayExpo[x][y] < -2){
+        data << " 184 164 0 ";
+      }
+      else if(arrayExpo[x][y] >= -2 && arrayExpo[x][y] < -1.7){
+        data << " 196 181 0 ";
+      }
+      else if(arrayExpo[x][y] >= -1.7 && arrayExpo[x][y] < -1.4){
+        data << " 208 198 0 ";
+      }
+      else if(arrayExpo[x][y] >= -1.4 && arrayExpo[x][y] < -1.1){
+        data << " 220 215 0 ";
+      }
+      else if(arrayExpo[x][y] >= -1.1 && arrayExpo[x][y] < -0.8){
+        data << " 232 232 0 ";
+      }
+      else if(arrayExpo[x][y] >= -0.5 && arrayExpo[x][y] < -0.2){
+        data << " 244 249 0 ";
+      }
+      else if(arrayExpo[x][y] >= -0.2 && arrayExpo[x][y] < 0){
+        data << " 255 255 0 ";
+      }
+
+      // LAMBDA POSITIF
+      else {
+        data << " 0 0 139 ";
+      }
     }
+    data << std::endl;
   }
+  data.close();
   return arrayExpo;
 }
 
-
 int main(){ // Revoir l'implémentation graphique de Lyapu mais semble fonctionnelle au vue de la tete du tableau en 10 10 sur AAB;
             // Implementation du code en PNM PORTABLE PIXMAP A FAIRE : APPRENDRE ECRITURE FICHIER + LECTURE
-  std::string seq = "AAB";
+  std::string seq = "BABAABAAAB";
   seq=AB(seq);
-  std::ofstream data("data.ppm"); // Création du fichier ou est stockée l'image
-  data << "P3";;
-  data << std::endl;
-  data << "600 600";
-  data << std::endl;
-  data << "255";
-  data << std::endl;
+  double ** arrayExpo=exposant(seq);
+  free(arrayExpo);
 
-  double ** arrayOfLyapu=exposant(seq);
-  for(int i=0;i<WIDTH;++i){
-    for(int j=0;j<HEIGHT;++j){
-      //std::cout << arrayOfLyapu[i][j];
-      if(arrayOfLyapu[i][j] < -2){
-        data << " 184 134 11 ";
-      }
-      else if(arrayOfLyapu[i][j] >= -2 && arrayOfLyapu[i][j] < -1){
-        data << " 255 215 0 ";
-      }
-
-      else if(arrayOfLyapu[i][j] >= -1 && arrayOfLyapu[i][j] < -0.5){
-        data << " 255 228 54 ";
-      }
-      else if(arrayOfLyapu[i][j] >=-0.5 && arrayOfLyapu[i][j] <0 ){
-        data << " 255 255 0";
-      }
-      else {
-        data << " 0 0 255";
-      }
-    }
-    //std::cout << std::endl;
-    data << std::endl;
-  }
-  free(arrayOfLyapu);
-  data.close();
   return EXIT_SUCCESS;
 }
